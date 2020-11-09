7cc8ca66c6f08f6cfc11ade4f10c5a3ffc540b10
LCKway <lckwat@qq.com>
2020-11-09 17:38:25 +0800

    上传一个项目

---
+++ U_W25Q32/MDK-ARM/RTE/_U_W25Q32/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'U_W25Q32' 
+ * Target:  'U_W25Q32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ U_W25Q32/MDK-ARM/U_W25Q32/startup_stm32f411xe.d
@@ -0,0 +1,1 @@
+u_w25q32\startup_stm32f411xe.o: startup_stm32f411xe.s
